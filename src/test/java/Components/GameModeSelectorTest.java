167824021a7af2e901a3490695ecef8e0d53c74d
Drew-Childs <60235260+Drew-Childs@users.noreply.github.com>
2022-11-06 01:13:58 -0500

    Updating name of tests

--- src/test/java/Components/GameModeSelectorTest.java
+++ src/test/java/Components/GameModeSelectorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GameLogicSelectorTest {
+public class GameModeSelectorTest {
     JFXPanel panel = new JFXPanel();
 
     @Test
